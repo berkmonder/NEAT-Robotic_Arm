ad717c84f0d7e9fa22be18b61cc4e82a636c242b
berkmonder <berkmonder@yahoo.com>
2022-05-24 22:15:39 +0300

    init

---
+++ main.py
@@ -0,0 +1,62 @@
+import multiprocessing
+import os
+import pickle
+import neat
+import numpy as np
+
+import gym
+
+runs_per_net = 2
+
+def eval_genome(genome, config):
+    net = neat.nn.RecurrentNetwork.create(genome, config)
+
+    fitnesses = []
+
+    for runs in range(runs_per_net):
+        env = gym.make("Acrobot-v1")
+
+        observation = env.reset()
+
+        fitness = 0.0
+        done = False
+        while not done:
+
+            action = np.argmax(net.activate(observation))
+            observation, reward, done, info = env.step(action)
+            fitness += reward
+
+        fitnesses.append(fitness)
+
+    return np.mean(fitnesses)
+
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = eval_genome(genome, config)
+
+
+def run():
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    pop = neat.Population(config)
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.StdOutReporter(True))
+
+    pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), eval_genome)
+    winner = pop.run(pe.evaluate)
+
+    # Save the winner.
+    with open('winner', 'wb') as f:
+        pickle.dump(winner, f)
+
+    print(winner)
+
+
+if __name__ == '__main__':
+    run()
